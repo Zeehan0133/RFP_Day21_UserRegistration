2d6342a49e5e106a5d6a29d8b0920d9b9e0b68f5
Zeehan0133 <zyne0133@gmail.com>
2021-10-19 13:49:02 +0530

    This is main program class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Day21_UserRegistration_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+        }
+    }
+}
